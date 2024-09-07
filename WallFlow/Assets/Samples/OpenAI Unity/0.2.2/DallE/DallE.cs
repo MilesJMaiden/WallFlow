3bcbf440a643bf545f2b42319eb975255b4b01d1
HjoonJ <150797637+HjoonJ@users.noreply.github.com>
2024-09-07 19:22:25 +0100

    www

--- WallFlow/Assets/Samples/OpenAI Unity/0.2.2/DallE/DallE.cs
+++ WallFlow/Assets/Samples/OpenAI Unity/0.2.2/DallE/DallE.cs
@@ -12,6 +12,7 @@ namespace OpenAI
         [SerializeField] private Button button;
         [SerializeField] private Image image;
         [SerializeField] private GameObject loadingLabel;
+        [SerializeField] private GameObject imagePrefab; // 이미지 복사본을 넣을 프리팹
 
         private OpenAIApi openai = new OpenAIApi();
 
@@ -55,6 +56,9 @@ namespace OpenAI
                     texture.LoadImage(request.downloadHandler.data);
                     var sprite = Sprite.Create(texture, new Rect(0, 0, 256, 256), Vector2.zero, 1f);
                     image.sprite = sprite;
+
+                    // 이미지 생성이 완료되면 새로운 이미지 오브젝트를 생성
+                    CreateImageCopy(sprite);
                 }
             }
             else
@@ -65,6 +69,29 @@ namespace OpenAI
             button.enabled = true;
             inputField.enabled = true;
             loadingLabel.SetActive(false);
+        }
+
+        // 이미지를 복사하여 새로운 오브젝트를 생성하는 메서드
+        private void CreateImageCopy(Sprite originalSprite)
+        {
+            // 이미지가 복사될 새로운 오브젝트 생성
+            GameObject newImageObject = Instantiate(imagePrefab);
+
+            // 새 오브젝트의 이미지 컴포넌트에 복사된 스프라이트 할당
+            Image newImageComponent = newImageObject.GetComponent<Image>();
+            if (newImageComponent != null)
+            {
+                newImageComponent.sprite = originalSprite;
+                Debug.Log("New image object created with copied sprite.");
+            }
+            else
+            {
+                Debug.LogError("The imagePrefab does not have an Image component.");
+            }
+
+            // 새 이미지 오브젝트의 위치나 기타 설정을 추가할 수 있음
+            newImageObject.transform.SetParent(transform.parent); // 부모를 설정하거나
+            newImageObject.transform.position += new Vector3(300, 0, 0); // 원래 이미지 옆에 위치시킴
         }
     }
 }
